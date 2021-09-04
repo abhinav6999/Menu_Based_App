d3d6d8ef19c3de933f04ec7d4858558765ee1be5
abhinav6999 <485abhinavsingh@gmail.com>
2021-09-04 17:29:05 +0530

    chrome branch created

--- Menu.java
+++ Menu.java
@@ -34,5 +34,15 @@ public class Menu{
             System.out.println("Error: " + Ex.toString());
         }
     }
+    static void chrome(){
+        Runtime app = Runtime.getRuntime();
+        try{
+            app.exec("chrome");
+           
+        }
+        catch (Exception Ex){
+            System.out.println("Error: " + Ex.toString());
+        }
+    }
 
 }
