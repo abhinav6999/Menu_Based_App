5fe6edd59d1aaf50cb7f0d439053ebeb0b0718b4
abhinav6999 <485abhinavsingh@gmail.com>
2021-09-04 17:20:01 +0530

    notepad branch created

---
+++ Menu.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Menu{
+    public static void main(String[] args){
+        System.out.println("Enter the number through menu list:");
+        Scanner input=new Scanner(System.in);
+        int numeric=input.nextInt();
+        if(numeric==1){
+            notepad();
+        }
+        else if(numeric==2){
+            calc();
+        }
+        else if(numeric==3){
+            chrome();
+        }
+    }
+    static void notepad(){
+        Runtime app = Runtime.getRuntime();
+        try{
+            app.exec("notepad");
+           
+        }
+        catch (Exception Ex){
+            System.out.println("Error: " + Ex.toString());
+        }
+    }
+
+}
